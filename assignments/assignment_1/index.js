f77f2c31f05ad6e516f8919606f3c763b65d000c
polipalliDevi <deviramakrishnaa8101@gmail.com>
2022-02-20 12:35:59 +0530

    myAssignment1_final

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,12 +1,28 @@
+const { stdin, stdout } = require("process")
+
 function getNameFromCommandLine() {
+   
+    const name=process.argv
+
+
+    return name[name.length-1]
     // Write you code here, name should be taken as args in process.argv
 }
 
 function getNameFromEnv() {
+    process.env.NODE_ENV="Yash"
+    
+     return process.env.NODE_ENV
     // Write your code here
 }
 
 function getNameFromReadLine() {
+    const readline = require('readline');
+    readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    
     // Write your code here
 }
 
